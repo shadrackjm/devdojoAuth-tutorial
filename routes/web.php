5a6d946e2976d27b8036be3bd9fe10f3f11d3d64
shadrackjm <shadrackmballah80@gmail.com>
2024-07-21 20:56:55 +0300

    an intro to dev dojo auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/home',[AdminController::class,'loadAdminHome'])->middleware('admin');
+Route::get('/user/home',[UserController::class,'loadUserHome'])->middleware('user');
